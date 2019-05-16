70f65fa8cbb3c999a9a370d4d377884700bf6a53
mcostafilho <mauro.lcf@gmail.com>
2019-05-16 18:39:41 -0300

    Update CollegeValidator.java

--- back/src/main/java/br/senac/pi06/validator/CollegeValidator.java
+++ back/src/main/java/br/senac/pi06/validator/CollegeValidator.java
@@ -9,8 +9,17 @@ public class CollegeValidator {
 
 		// NOT NULL
 		if (Util.empty(college.getName()))
-			return new CollegeException("o nome eh obrigatorio.");
+			return new CollegeException("O nome da instuição é obrigatorio.");
 
+		if (Util.empty(college.getTipo()))
+			return new CollegeException("O tipo da instuição é obrigatorio.");
+		
+		if (Util.empty(college.getCidade()))
+			return new CollegeException("A cidade é obrigatoria.");
+
+		if (Util.empty(college.getEstado()))
+			return new CollegeException("O estado é obrigatorio.");
+		
 		return null;
 
 	}
