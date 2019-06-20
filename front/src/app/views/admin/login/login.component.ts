f60ded4fa780d3525272cc6df0eafcfc9e81f340
PauloLNavarro <navarro.win@gmail.com>
2019-06-20 00:48:02 -0300

    fix login

--- front/src/app/views/admin/login/login.component.ts
+++ front/src/app/views/admin/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { Router } from '@angular/router';
-
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -10,8 +10,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  erroMsg: string = null;
   loginForm: FormGroup;
+  submitted = false;
 
   constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) { }
 
@@ -24,22 +25,40 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-    let user: any = {
-      username: null,
-      passwd: null,
-    };
 
-    user.username = this.loginForm.value.username;
-    user.passwd = this.loginForm.value.passwd;
+    this.submitted = true;
+    this.erroMsg = null;
+    if (!this.loginForm.invalid) {
+      let user: any = {
+        username: null,
+        passwd: null,
+      };
 
-    this.auth.login(user).subscribe((user) => {
-      localStorage.setItem('token', user.token);
-      this.router.navigate(['/admin']);
-    });
+      user.username = this.loginForm.value.username;
+      user.passwd = this.loginForm.value.passwd;
+
+      this.auth.login(user).subscribe((user) => {
+        localStorage.setItem('token', user.token);
+        this.router.navigate(['/admin']);
+      },
+        (err: HttpErrorResponse) => {
+          switch (err.status) {
+            case 403:
+              this.erroMsg = "Usuário ou senha inválida!";
+              this.loginForm.reset();
+              break;
+            default:
+              this.erroMsg = "Falha ao tentar realizar o login, recarregue a pagina!";
+              break;
+          }
+        })
+      this.submitted = false;
+    }
   }
 
   get formLogin() {
     return this.loginForm.controls;
   }
+
 
 }
