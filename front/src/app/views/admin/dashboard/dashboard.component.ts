99a0323f382b6b510752a5ed648633838dcee1a4
PauloLNavarro <navarro.win@gmail.com>
2019-06-27 23:25:23 -0300

    fix

--- front/src/app/views/admin/dashboard/dashboard.component.ts
+++ front/src/app/views/admin/dashboard/dashboard.component.ts
@@ -214,35 +214,5 @@ export class DashboardComponent implements OnInit {
   public chartClicked03(e: any): void { }
   public chartHovered03(e: any): void { }
 
-  // Grafico 04
-  public chartType04: string = 'radar';
-
-  public chartDatasets04: Array<any> = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'My First dataset' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'My Second dataset' }
-  ];
-
-  public chartLabels04: Array<any> = ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'];
-
-  public chartColors04: Array<any> = [
-    {
-      backgroundColor: 'rgba(105, 0, 132, .2)',
-      borderColor: 'rgba(200, 99, 132, .7)',
-      borderWidth: 2,
-    },
-    {
-      backgroundColor: 'rgba(0, 250, 220, .2)',
-      borderColor: 'rgba(0, 213, 132, .7)',
-      borderWidth: 2,
-    }
-  ];
-
-  public chartOptions04: any = {
-    responsive: true
-  };
-
-  public chartClicked04(e: any): void { }
-  public chartHovered04(e: any): void { }
-
 
 }
